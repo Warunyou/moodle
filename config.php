e1f661d1ff88fec378625f52c67aa99722038078
Warunyou <39452360+Warunyou@users.noreply.github.com>
2020-11-16 16:57:39 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dbtype    = getenv('pgsql');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv('ec2-3-233-236-188.compute-1.amazonaws.com');
+$CFG->dbname    = getenv('d9bmikti5oh7r0');
+$CFG->dbuser    = getenv('vubugeunhoovpp');
+$CFG->dbpass    = getenv('2b9b041c22f0ac098f5fe725e66bedac2a16998aba5a0c98f21b12842e87ef24');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => getenv('5432'),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('WWWROOT');
-$CFG->dataroot  = getenv('DATAROOT');
+$CFG->wwwroot   = getenv('http://ska-assist.herokuapp.com/');
+$CFG->dataroot  = getenv('try /tmp');
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
